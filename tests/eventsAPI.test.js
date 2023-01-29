322c985bc7e3505ae735d04800de2f7dde682488
aida <aida.denisa@gmail.com>
2023-01-29 18:47:19 +0200

    Updated tests with an example of beforeALL/beforeEach

--- tests/eventsAPI.test.js
+++ tests/eventsAPI.test.js
@@ -8,6 +8,33 @@ import app from '../app.js';
 // by wrapping it in a superagent object
 const api = supertest(app);
 
+/** An example of how you can initialize the DB. 
+ * In the current case it's not needed because these are seeds.s
+
+import Event from '../models/event.js';
+import Category from '../models/category.js';
+import { categories, events } from '../db/seeds.js';
+
+beforeAll(async () => {
+  // Delete current categories and events in the DB
+  // * Important: when we await a Mongoose operation, we need to use .exec()
+  await Event.deleteMany({ name: { $exists: true } }).exec();
+  await Category.deleteMany({ name: { $exists: true } }).exec();  
+
+  const newCategories = await Category.insertMany(
+    categories.map(c => ({ ...c, code: c.name.toUpperCase() }))
+  );
+  
+  for(let key in events) {
+    const categoryId = newCategories.find(c => c.code === key)._id.toString();
+    const newEvents = events[key].map(e => ({ ...e, category: mongoose.Types.ObjectId(categoryId), code: e.name.toUpperCase() }))
+    console.log(newEvents);
+
+    await Event.insertMany(newEvents);
+  }
+})
+*/
+
 test('events are returned as JSON', async () => {
   await api
     .get('/events')
@@ -20,7 +47,7 @@ test('events list is not empty', async () => {
 
   // we use JEST here to test the correctness https://jestjs.io/docs/using-matchers
   expect(result.body).not.toBeUndefined();
-  expect(result.body.length).toBeGreaterThan(4);
+  expect(result.body.length).toBeGreaterThan(1);
 })
 
 afterAll(() => {
