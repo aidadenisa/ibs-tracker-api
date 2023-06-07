e41e130e1d9640345833b2cb56f767090a3fa61b
aida <aida.denisa@gmail.com>
2023-06-07 18:07:33 +0530

    Added try/catch to all controllers

--- controllers/events.js
+++ controllers/events.js
@@ -3,9 +3,11 @@ import eventsService from '../services/events.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const result = await eventsService.listEvents();
-  res.json(result);
+router.get('/', async (req, res, next) => {
+  try {
+    const result = await eventsService.listEvents();
+    res.json(result).status(200);
+  } catch (err) { next(err) };
 })
 
 export default router;
--- controllers/records.js
+++ controllers/records.js
@@ -6,31 +6,40 @@ const router = express.Router();
 
 // TODO: Replace this after adding auth with logged in user's credentials
 router.get('/:userId', async (req, res, next) => {
-  const result = await recordsService.listRecordsByUserId(req.params.userId, req.query.populate);
-  logger.info(result)
-  res.json(result);
+  try {
+    const result = await recordsService
+      .listRecordsByUserId(req.params.userId, req.query.populate);
+    logger.info(result)
+    res.json(result);
+  } catch (err) { next(err) };
 });
 
 router.post('/', async (req, res, next) => {
   const data = req.body;
-  if(!data || !data.userId || !data.eventId) {
+  if (!data || !data.userId || !data.eventId) {
     return res.status(400).json({ error: 'Missing required properties: userId or eventId' })
   }
-  const result = await recordsService.createNewRecord(data);
-  res.json(result);
+  try {
+    const result = await recordsService.createNewRecord(data);
+    res.json(result);
+  } catch (err) { next(err) };
 });
 
 router.put('/:id', async (req, res, next) => {
-  if(!req.body && !req.body.eventId) {
+  if (!req.body && !req.body.eventId) {
     res.status(400).json({ error: 'You need to specify an event for updating the record.' })
   }
-  const result = await recordsService.updateRecordProperties(req.body);
-  res.json(result);
+  try {
+    const result = await recordsService.updateRecordProperties(req.body);
+    res.json(result);
+  } catch (err) { next(err) };
 })
 
 router.delete('/:id', async (req, res, next) => {
-  await recordsService.deleteRecord(req.params.id);
-  res.status(204).end();
+  try {
+    await recordsService.deleteRecord(req.params.id);
+    res.status(204).end();
+  } catch (err) { next(err) };
 })
 
 export default router;
