095ce3569c2b3803ac84027b4a6b7c174c1937d9
aida <aida.denisa@gmail.com>
2023-06-21 23:48:15 +0200

    Updated tests to make them pass

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,7 +3,6 @@ import logger from '../utils/logger.js';
 // Middleware that manages error handling in one place
 // This is an Express error handler, that has 4 params
 const errorHandler = (error, request, response, next) => {
-  logger.error(error.message);
 
   if(error.name === 'CastError') {
     return response.status(400).send({ error: 'Malformed id' });
